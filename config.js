ff8223403aca844d40e317446966e6335800bcc1
Cuong2109 <85282510+Cuong2109@users.noreply.github.com>
2023-04-16 14:56:26 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Trang ơi!',
+    introDesc: `Em có đang rảnh không?`,
+    btnIntro: 'Em có',
+    title: 'Vậy anh hỏi em câu này nhé ❤',
+    desc: 'Em có yêu anh khôngggg? ',
+    btnYes: 'Em có',
+    btnNo: 'Em ghét anh',
+    question:'Trời đất ơi!!!Tại sao lại yêu anh <3',
+    btnReply: 'Trả lời anh đi <3',
+    reply: 'Đừng cố viết gì cả anh biết mà',
+    mess: 'Yêu em ❤❤❤',
+    messDesc: 'Vậy thì tin nhắn cho anh đi chứ',
+    btnAccept: 'Gửi❤❤❤',
+    messLink: 'http://www.facebook.com/messages/t/100006317044070' //link mess
+}
